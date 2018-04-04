e262801c4ecc63a933901670879be3668eb9ed76
Daan Treureniet <dptreurniet@gmail.com>
2018-04-04 15:03:28 +0200

    Added prototypes for controller classes

---
+++ printjet_software/main_controller.py
@@ -0,0 +1,25 @@
+# Written by Daan Treurniet
+
+# This is the main controller of the printjet system,
+# which controls all aspects of the printer
+
+
+from motion_stage_controller import MotionStageController
+from printhead_controller import PrintheadController
+
+
+class MainController():
+    def __init__(self):
+        self.motion_stage_controller = MotionStageController()
+        self.printhead_controller = PrintheadController()
+        print('Main Controller initialized...'')
+
+    def main_loop(self):
+        pass
+
+
+
+
+if __name__ == '__main__':
+    main_controller = MainController()
+    main_controller.main_loop()
---
+++ printjet_software/motion_stage_controller.py
@@ -0,0 +1,13 @@
+class MotionStageController():
+    """
+    This class takes care of controller the motion stage.
+    """
+
+
+    def __init__(self):
+        self.current_pos = (0, 0)
+        print('Motion Stage Controller initialized...')
+
+    def move_to_position(position):
+        """Move motion stage to position"""
+        pass
