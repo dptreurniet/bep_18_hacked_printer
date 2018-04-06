81e84ed15a2af2b530504aa2d1a869c406cb2229
Daan Treureniet <dptreurniet@gmail.com>
2018-04-06 09:46:28 +0200

    Added printhead controller

---
+++ printjet_software/printhead_controller.py
@@ -0,0 +1,8 @@
+class PrintheadController():
+    """
+    This class takes care of controlling the printhead.
+    """
+
+
+    def __init__(self):
+        print('Printhead Controller initialized...')
