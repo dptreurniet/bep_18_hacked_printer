6b4a1b2dfbbfaf0babecc05e0cd3025d00b9d5f3
Daan Treureniet <dptreurniet@gmail.com>
2018-05-02 11:06:05 +0200

    Added config file

---
+++ pyboard/main/config.py
@@ -0,0 +1,29 @@
+
+
+################## Pin definitions ###################
+
+STEP_ENA =      'X8'
+
+# X stage
+X_DIR =         'X1'
+X_STEP =        'X2'
+X_END_MIN =     'X4'
+X_END_MAX =     'X3'
+
+# Y stage
+Y_DIR =         'X9'
+Y_STEP =        'X10'
+Y_END_MIN =     'X12'
+Y_END_MAX =     'X11'
+
+
+# LV printhead logic
+SICL =          'Y1'
+SIBL =          'Y2'
+CK =            'Y3'
+LAT =           'Y4'
+CH =            'Y5'
+NCHG =          'Y6'
+
+# DAC
+DAC =           'X5'
